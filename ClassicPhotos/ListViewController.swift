6c0d560248dc9244b6a1deaf453cb63575740396
yjkim <yjkim8202@gmail.com>
2015-11-03 22:17:51 +0900

    working

--- ClassicPhotos/ListViewController.swift
+++ ClassicPhotos/ListViewController.swift
@@ -12,61 +12,98 @@ import CoreImage
 let dataSourceURL = NSURL(string:"http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist")
 
 class ListViewController: UITableViewController {
-  
-  lazy var photos = NSDictionary(contentsOfURL:dataSourceURL!)!
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.title = "Classic Photos"
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  // #pragma mark - Table view data source
-  
-  override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
-    return photos.count
-  }
-  
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath) 
-    let rowKey = photos.allKeys[indexPath.row] as! String
     
-    var image : UIImage?
-    if let imageURL = NSURL(string:photos[rowKey] as! String),
-    imageData = NSData(contentsOfURL:imageURL){
-      //1
-      let unfilteredImage = UIImage(data:imageData)
-      //2
-      image = self.applySepiaFilter(unfilteredImage!)
+    //lazy var photos = NSDictionary(contentsOfURL:dataSourceURL!)!
+    var photos = [PhotoRecord]()
+    let pendingOperations = PendingOperations()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Classic Photos"
+        
+        fetchPhotoDetails()
     }
     
-    // Configure the cell...
-    cell.textLabel?.text = rowKey
-    if image != nil {
-      cell.imageView?.image = image!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    return cell
-  }
-  
-  
-  func applySepiaFilter(image:UIImage) -> UIImage? {
-    let inputImage = CIImage(data:UIImagePNGRepresentation(image)!)
-    let context = CIContext(options:nil)
-    let filter = CIFilter(name:"CISepiaTone")
-    filter?.setValue(inputImage, forKey: kCIInputImageKey)
-    filter?.setValue(0.8, forKey: "inputIntensity")
-    if let outputImage = filter?.outputImage {
-      let outImage = context.createCGImage(outputImage, fromRect: outputImage.extent)
-      return UIImage(CGImage: outImage)
-    }
-    return nil
+    // #pragma mark - Table view data source
     
-  }
-  
+    override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        
+        //1
+        if cell.accessoryView == nil {
+            let indicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+            cell.accessoryView = indicator
+        }
+        let indicator = cell.accessoryView as! UIActivityIndicatorView
+        
+        //2
+        let photoDetails = photos[indexPath.row]
+        
+        //3
+        cell.textLabel?.text = photoDetails.name
+        cell.imageView?.image = photoDetails.image
+        
+        //4
+        switch (photoDetails.state){
+        case .Filtered:
+            indicator.stopAnimating()
+        case .Failed:
+            indicator.stopAnimating()
+            cell.textLabel?.text = "Failed to load"
+        case .New, .Downloaded:
+            indicator.startAnimating()
+            self.startOperationsForPhotoRecord(photoDetails,indexPath:indexPath)
+        }
+        
+        return cell
+    }
+    
+    
+    func fetchPhotoDetails() {
+        let request = NSURLRequest(URL:dataSourceURL!)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {response,data,error in
+            if data != nil {
+                let datasourceDictionary = NSPropertyListSerialization.propertyListWithData(data, options: Int(NSPropertyListMutabilityOptions.Immutable.rawValue), format: nil, error: nil) as! NSDictionary
+                
+                for(key : AnyObject,value : AnyObject) in datasourceDictionary {
+                    let name = key as? String
+                    let url = NSURL(string:value as? String ?? "")
+                    if name != nil && url != nil {
+                        let photoRecord = PhotoRecord(name:name!, url:url!)
+                        self.photos.append(photoRecord)
+                    }
+                }
+                
+                self.tableView.reloadData()
+            }
+            
+            if error != nil {
+                let alert = UIAlertView(title:"Oops!",message:error.localizedDescription, delegate:nil, cancelButtonTitle:"OK")
+                alert.show()
+            }
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        }
+    }
+    
+    func startOperationsForPhotoRecord(photoDetails: PhotoRecord, indexPath: NSIndexPath){
+        switch (photoDetails.state) {
+        case .New:
+            startDownloadForRecord(photoDetails, indexPath: indexPath)
+        case .Downloaded:
+            startFiltrationForRecord(photoDetails, indexPath: indexPath)
+        default:
+            NSLog("do nothing")
+        }
+    }
 }
